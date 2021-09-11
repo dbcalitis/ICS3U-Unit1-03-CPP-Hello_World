f5742cefeb90bf6db0b50a92b1748d9e25e358ad
Ubuntu <ubuntu@ip-172-31-81-123.ec2.internal>
2021-09-11 00:42:02 +0000

    proper code style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 D.B. Calitis All rights reserved
+//
+// Created by: Daria Bernice Calitis
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!";
+}
